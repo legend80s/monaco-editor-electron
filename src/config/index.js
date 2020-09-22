f6ca5e8e8ccbc2f5590d382f83571157f10c6038
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2020-09-22 21:55:53 +0800

    feat: first commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,31 @@
+import isElectron from 'is-electron';
+import path from 'path';
+
+function ensureFirstBackSlash(str) {
+  return str.length > 0 && str.charAt(0) !== '/'
+    ? '/' + str
+    : str;
+}
+
+function uriFromPath(_path) {
+  const pathName = path.resolve(_path).replace(/\\/g, '/');
+
+  return encodeURI('file://' + ensureFirstBackSlash(pathName));
+}
+
+const vsElectronPath = uriFromPath(
+  path.join(__dirname, '../node_modules/monaco-editor/min/vs')
+);
+
+const vs = isElectron()
+  ? vsElectronPath
+  : 'https://cdn.jsdelivr.net/npm/monaco-editor@0.20.0/min/vs'
+
+// console.log('[monaco-editor-electron] using vs:', vs);
+
 const config = {
   paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.20.0/min/vs',
+    vs,
   },
 }
 
